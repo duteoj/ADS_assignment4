3805b7e17d8324f2293a0b919f9659038112a2f9
duteoj <145760725+duteoj@users.noreply.github.com>
2026-05-22 23:07:54 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,21 @@ public class Main {
 
         System.out.println("\n--- Performance Comparison Table ---");
         experiment.printResults();
+
+        WeightedGraph wg = new WeightedGraph();
+        for (int i = 0; i < 6; i++) {
+            wg.addVertex(new Vertex(i));
+        }
+        wg.addEdge(0, 1, 2);
+        wg.addEdge(1, 2, 3);
+        wg.addEdge(0, 3, 6);
+        wg.addEdge(1, 4, 8);
+        wg.addEdge(2, 5, 7);
+        wg.addEdge(3, 4, 1);
+        wg.addEdge(4, 5, 5);
+
+        wg.printGraph();
+        wg.dijkstra(0);
+
     }
 }
