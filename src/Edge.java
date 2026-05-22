6f5381db729005ca89e95ac21a7a132a0d6491c2
duteoj <145760725+duteoj@users.noreply.github.com>
2026-05-22 23:09:31 +0500

    Update Edge.java

--- src/Edge.java
+++ src/Edge.java
@@ -1,10 +1,18 @@
 public class Edge {
     private Vertex source;
     private Vertex destination;
+    private int weight;
 
     public Edge(Vertex source, Vertex destination) {
         this.source = source;
         this.destination = destination;
+        this.weight = 1;
+    }
+
+    public Edge(Vertex source, Vertex destination, int weight) {
+        this.source = source;
+        this.destination = destination;
+        this.weight = weight;
     }
 
     public Vertex getSource() {
@@ -15,8 +23,12 @@ public class Edge {
         return destination;
     }
 
+    public int getWeight() {return weight;}
+
+
+
     @Override
     public String toString() {
-        return source + " -> " + destination;
+        return source + " -(" + weight + ")-> " + destination;
     }
 }
